eadcd278ada3c1f5604660f83dbb9e70c8459746
Rschen7754 <rschen7754.wiki@gmail.com>
2014-04-01 21:59:41 -0700

    connecting code

--- projects.php
+++ projects.php
@@ -1,6 +1,6 @@
 <html>
 <head><title>Projects</title>
-	<style type="text/css">@import "blue/style.css";</style>
+<style type="text/css">@import "blue/style.css";</style>
 <script type="text/javascript" src="jquery-latest.js"></script> 
 <script type="text/javascript" src="jquery.tablesorter.js"></script> 
 <script type="text/javascript">
@@ -17,12 +17,24 @@ This will be a replacement for erwin85's projects tool.
 <br /><br />
 <table id="projects" class="tablesorter">
 <thead>
-<tr><th>Wiki</th><th>Admin</th><th>Crat</th><th>CU</th><th>OS</th></tr>
+<tr><th>Wiki</th><th>sysop</th><th>bureaucrat</th><th>checkuser</th><th>oversight</th></tr>
 </thead>
 <tbody>
-<tr><td>enwiki</td><td>0</td><td>0</td><td>5</td><td>0</td></tr>
-<tr><td>dewiki</td><td>1</td><td>0</td><td>3</td><td>0</td></tr>
+	<?php
+	require_once 'login.php';
+	$db_server = mysql_connect("metawiki.labsdb", $db_username, $db_password);
+
+	if (!db_server) die ("Unable to connect to MySQL: " . mysql_error());
+
+	mysql_select_db("meta_p", $db_server) or die ("Unable to select database: " . mysql_error());
+
+
+	?>
+
 </tbody>
 </table>
+<br />
+<p>Acknowledgements to erwin85 for the original tool, and to Pathoschild for creating the 
+extensive suite of tools that I used as an example.</p>
 </body>
 </html>
