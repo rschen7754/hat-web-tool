cd98a90ce772a16f67887b5e10cc1b7d38766a25
Rschen7754 <rschen7754.wiki@gmail.com>
2014-04-01 19:33:00 -0700

    add table

--- projects.php
+++ projects.php
@@ -2,5 +2,10 @@
 <head><title>Projects</title></head>
 <body>
 This will be a replacement for erwin85's projects tool.
+
+<br /><br />
+<table>
+<tr><th>Wiki</th><th>Admin</th><th>Crat</th><th>CU</th><th>OS</th></tr>
+</table>
 </body>
 </html>
