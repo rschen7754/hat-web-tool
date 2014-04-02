a587b7d3a412fe4a60a98432b9887de738bf9e36
Rschen7754 <rschen7754.wiki@gmail.com>
2014-04-01 22:21:07 -0700

    broken links

--- projects.php
+++ projects.php
@@ -28,7 +28,7 @@ This will be a replacement for erwin85's projects tool.
 
 	mysql_select_db("meta_p", $db_server) or die ("Unable to select database: " . mysql_error());
 
-	$query = "SELECT dbname,REPLACE(url, 'http://', '') AS domain, slice FROM wiki WHERE url IS NOT NULL AND is_closed=0;";
+	$query = "SELECT dbname,REPLACE(url, 'http://', 'https://') AS domain, slice FROM wiki WHERE url IS NOT NULL AND is_closed=0;";
 	$result = mysql_query($query);
 	
 	if (!$result) die ("Database access failed: " . mysql_error());
